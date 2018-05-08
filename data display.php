d6710c611664a125c517097d1e81b5b5ec0b5de5
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-05-08 10:38:23 +0300

    Add database

---
+++ data display.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<tiltle> Display data into database</title>
+<style type= "text/css">
+table{
+	border:2px solid red;
+	backgroungd color:#FFC
+	}
+th	{ 
+		border-bottom 5px solid #000
+	}
+td	{
+	border-bottom: 2px solid #666
+	}
+</style>
+</head>	
+<body>
+<h1>display data from database</h1>
+<?php
+
+include('connect-mysql.php');
+
+$sqlget="SELECT*FROM customer";
+$sqldata=mysqli_query($dbcon,$sqlget) or die('ertror getting data')
+
+echo"<table>";
+echo"<tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>";
+
+while ($row=mysqli_fetch_array($sqldata,MYSQLI_ASSOC)){
+	echo"<tr><td>";
+	echo$row['customerID'];
+	echo"</td><td>";
+	echo$row['customerFName'];
+	echo"</td><td>";
+	echo"</td></tr>";
+	}
+echo"</table>";
+	
+</body>
+</html>
---
+++ data insertion.php
@@ -0,0 +1,36 @@
+<?php
+if (isset($_POST['submitted'])){
+	include('connect-mysql.php');
+	$firstName=$_POST['firstName'];
+	$lastName=$_POST['lastName'];
+	$customerNumber=$_POST['customerNumber']
+	$sqlinsert="INSERT INTO customer(customerFName,customerLName,customerNO) VALUES('$firstName','$lastName','$customerNumber')";
+	
+	if(!mysqli_query($dbconnection,$sqlinsert)){
+		die('error detected while inserting new record')
+	}
+		$newrecord= "one recored added to the database";
+?>
+
+<html>
+<head>
+<tiltle> insert data to database</title>
+</head>
+<body>
+<h1>insert data into database</h1>
+<form method="POST" action="insert-data.php">
+<input type="hidden" name="submitted" value="true"/>
+<fieldset>
+	<legend> new people</legend>
+	<label>first name:input type="text" name= "fname"/></label>
+	<label>last name:input type="text" name= "lname"/></label>
+	</fieldset>
+	<br/>
+	input type="submit" value="add new person/>
+	
+</form>
+<?php
+echo $new record 
+?>
+</body>
+</html>
