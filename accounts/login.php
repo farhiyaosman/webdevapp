f2702a3cacc1418458e4a3e187e479a3fa781638
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-05-14 15:27:56 +0300

    [Edit] - Change of location

---
+++ accounts/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<centre>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel-sheet="stylesheet" type="text/css" href="static/css/style.css">
+</head>
+<body>
+    <div id="form">
+    	<form action="" method="POST">
+
+    		<p>
+        		<label>Email:</label>
+				<input type="text" id="user" name="user" />
+      		</p>
+       		<p>
+        		<label>Password:</label>
+				<input type="password" id="pass" name="pass" />
+      		</p>
+			<p>
+				<input type="submit" id="btn" value="Submit" />
+			</p>
+
+		</form>
+	</div>
+</body>
+	</centre>
+</html>
---
+++ accounts/signup.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+    <center>
+<head>
+	<title>Sign Up</title>
+</head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+<body>
+    <div class="form">
+		<form action="" method="POST">
+			<h1>
+				<i>Sign Up</i>
+			</h1>
+			<table>
+				<tr>
+					<td>
+						 First Name:
+					</td>
+					<td>
+						<input type="text"placeholder="First Name" "name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Surname:
+					</td>
+					<td>
+						<input type="Surname"placeholder="Surname" "name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Password:
+					</td>
+					<td>
+						<input type="password" placeholder="Password""name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+					  Confirm Password:
+					</td>
+					<td>
+					   <input type="password" placeholder=" Confirm Password""name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Gender:
+					</td>
+					<td>
+						<input type="radio" name="gender">Male
+						<input type="radio" name="gender">Female
+						<input type="radio" name="gender">Other
+
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Email:
+					</td>
+					<td>
+						<input type="mail"placeholder="Email""name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Country
+					</td>
+					<td>
+						<input type="Country"placeholder="Country""name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Phone number:
+					</td>
+					<td>
+						<select>
+							<option>+254</option>
+							<option>+255</option>
+							<option>+256</option>
+						</select>
+						<input type="phone number" placeholder="712345678""name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="submit" value="Submit">
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</body>
+      </center>
+</html>
